f50ae574ba871c71c069a04265142f48a00badd7
Ed Coyle <edcoyle86@gmail.com>
2024-09-24 13:42:05 -0400

    Fix fonts, etc

--- src/components/AccentHeader.svelte
+++ src/components/AccentHeader.svelte
@@ -2,10 +2,11 @@
 	import ThinAccent from "./accents/thinAccent.svelte";
 
     export let footer = false;
+    export let offset = true;
 </script>
 
 <header class="flex flex-row items-center mb-8 gap-4 overflow-hidden text-start max-w-680 lg:w-680">
-    <ThinAccent offset/>
+    <ThinAccent offset={offset}/>
     {#if footer}
         <span class="whitespace-nowrap flex-grow-0 font-semibold font-serif">
             <slot></slot>
